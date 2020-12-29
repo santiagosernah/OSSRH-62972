a4c00daec00c8034a2348564bd0f60c3e49b83d8
Michelle <michelly-gomez@hotmail.com>
2020-12-29 10:03:09 -0500

    Structure is changed

--- src/main/java/grupoexito/domain/transaction/elk/Header.java
+++ src/main/java/grupoexito/domain/transaction/elk/Header.java
@@ -8,10 +8,10 @@ import java.util.Date;
 
 @Data
 @Builder(toBuilder = true)
-public class Header implements Serializable {
+public class Header<T> implements Serializable {
     private final String transactionId;
     private final String applicationId;
     private final Date transactionDate;
-    private final FlexField flexField; //como tiparlo (Json)
+    private final T flexField; //como tiparlo (Json)
 
 }
